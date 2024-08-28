b198a1040dd2bd0b181a3b20bf0ae68446efa892
“Your <jbuech8@lsu.edugit config --global user.email jbuech8@lsu.edu>
2024-08-28 15:14:13 -0500

    Here are my circles

---
+++ sketch.js
@@ -0,0 +1,95 @@
+function setup() {
+
+    createCanvas(1495,815);
+    background(0);
+   
+ //Background Ellipses  
+    fill(255);
+    ellipse(width/2, height/2, 1280, 3200);
+    fill(0);
+    ellipse(width/2, height/2, 640, 1600);
+    fill(255);
+    ellipse(width/2, height/2, 320, 800);
+    fill(0);
+    ellipse(width/2, height/2, 160, 400);
+    fill(255);
+    ellipse(width/2, height/2, 80, 200);
+    fill(0);
+    ellipse(width/2, height/2, 40, 100);
+    fill(255);
+    ellipse(width/2, height/2, 20, 50);
+
+//Top Left Pattern
+    fill(255)
+    stroke(255)
+    ellipse(width/5, height/5, 320, 1280)
+    fill(0)
+    stroke(0)
+    ellipse(width/5, height/5, 160, 640)
+    fill(255)
+    ellipse(width/5, height/5, 80, 320)
+    fill(0)
+    ellipse(width/5, height/5, 40, 160)
+    fill(255)
+    ellipse(width/5, height/5, 20, 80)
+    fill(0)
+    ellipse(width/5, height/5, 10, 40)
+    fill(255)
+    ellipse(width/5, height/5, 5, 20)
+
+//Top Right Pattern 
+    fill(0)
+    ellipse(1100, height/3, 480, 560)
+    fill(255)
+    ellipse(1100, height/3, 240, 280)
+    fill(0)
+    ellipse(1100, height/3, 120, 160)
+    fill(255)
+    ellipse(1100, height/3, 60, 80)
+    fill(0)
+    ellipse(1100, height/3, 30, 40)
+    fill(255)
+    ellipse(1100, height/3, 15, 20)
+
+//Bottom Left Pattern
+    fill(0)
+    ellipse(width/3, 700, 160, 960)
+    fill(255)
+    ellipse(width/3, 700, 80, 480)
+    fill(0)
+    ellipse(width/3, 700, 40, 240)
+    fill(255)
+    ellipse(width/3, 700, 20, 120)
+    fill(0)
+    ellipse(width/3, 700, 10, 60)
+    fill(255)
+    ellipse(width/3, 700, 5, 30)
+
+//Bottom Left Left Pattern
+    fill(255)
+    stroke(255)
+    ellipse(width/10, 800, 320, 480) 
+    fill(0)
+    stroke(0)
+    ellipse(width/10, 800, 160, 240) 
+    fill(255)
+    ellipse(width/10, 800, 80, 120) 
+    fill(0)
+    ellipse(width/10, 800, 40, 60) 
+    fill(255)
+    ellipse(width/10, 800, 20, 30)  
+    
+//Bottom Right Pattern
+    fill(255)
+    stroke(255)
+    ellipse(1250, 775, 240, 320)
+    fill(0)
+    ellipse(1250, 775, 120, 160)
+    fill(255)
+    ellipse(1250, 775, 60, 80)
+    fill(0)
+    ellipse(1250, 775, 30, 40)
+    fill(255)
+    ellipse(1250, 775, 15, 20)
+
+}
